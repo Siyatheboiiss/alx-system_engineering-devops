05870d464f1a751fbac146ac3bb8408bb2e959b1
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-09-26 23:36:16 +0200

    Install compatible version of Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,19 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
+# This Puppet manifest installs Flask version 2.1.0 and the compatible Werkzeug version using pip3
 
 package { 'python3-pip':
   ensure => installed,
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.2',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep 2.1.2',
+  require => Package['python3-pip'],
 }
 
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
   unless  => '/usr/bin/pip3 show Flask | grep 2.1.0',
-  require => Package['python3-pip'],
+  require => Exec['install_werkzeug'],
 }
