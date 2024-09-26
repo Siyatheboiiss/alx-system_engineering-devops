3c5153860ae2d767ceaba27382268449488c8f41
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-09-26 23:57:04 +0200

    Enhance

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,26 +1,22 @@
 # Puppet manifest to configure SSH client settings
-# This sets up the SSH client to use a specific private key and disables password authentication.
+# This sets up the global SSH client configuration to use a specific private key and disables password authentication.
 
-class { 'ssh::client':
-  config_file => '/home/vagrant/.ssh/config',
-}
-
-file { '/home/vagrant/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => file,
   content => template('ssh/ssh_config.erb'),
-  owner   => 'vagrant',
-  group   => 'vagrant',
-  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/vagrant/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => '    IdentityFile ~/.ssh/school',
   match => '^    IdentityFile',
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/home/vagrant/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => '    PasswordAuthentication no',
   match => '^    PasswordAuthentication',
 }
